421dcff352452f159bb9900933e1c14cf659af62
Daniel <daniel@Daniels-MacBook-Pro.local>
2019-11-01 06:16:05 -0500

    split components

--- src/Components/AppHeader.js
+++ src/Components/AppHeader.js
@@ -1,49 +1,7 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Select, Typography, PageHeader } from 'antd';
-import { sortDirectionFetch, sortByFetch } from '../Actions/helpers';
-
-const Filters = ({ show }) => {
-    const { Text } = Typography;
-    const { Option } = Select;
-    const dispatch = useDispatch();
-    const direction = useSelector( state => state.sortDirection);
-
-    const sortDirectionChange = (direction) => {
-        dispatch(sortDirectionFetch(dispatch, direction));
-    };
-
-    const sortByChange = (sortBy) => {
-        dispatch(sortByFetch(dispatch, sortBy));
-    };
-
-    return (
-        <div className="filters">
-            {show ? 
-            <>
-            <div key="direction">
-                <Text>Sort Direction</Text>
-                <Select defaultValue={direction} style={{ width: 120 }} onChange={sortDirectionChange}>
-                    <Option value="asc">Asc</Option>
-                    <Option value="desc">Desc</Option>
-                </Select>
-            </div>
-            <div key="sortBy">
-                <Text>Sort By</Text>
-                <Select defaultValue="name" style={{ width: 120 }} onChange={sortByChange}>
-                    <Option value="name">Name</Option>
-                    <Option value="symbol">Symbol</Option>
-                    <Option value="market_cap">Market Cap</Option>
-                    <Option value="price">Price</Option>
-                </Select>
-            </div>
-            </>
-            : 
-            <></>
-            }
-        </div>
-    )
-};
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { PageHeader } from 'antd';
+import Filters from './Filters';
 
 const AppHeader = () => {
     const showFilters = useSelector(state => state.currentRoute);
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,55 +4,7 @@ import { Card, Skeleton, Descriptions, Badge } from 'antd';
 import { useLocation } from 'react-router-dom';
 import { fecthCryptoCurrencyDetails } from '../Actions/details';
 import { setNavUrl } from '../Actions/helpers';
-
-const DetailFormatted = ({ data = {}}) => {
-    if(Object.keys(data).length === 0 ) {
-        return (<>No data available</>);
-    } else {
-        return (
-            <>
-            <Descriptions title="OxBitcoin" bordered>
-                <Descriptions.Item label="Name">{data.name}</Descriptions.Item>
-                <Descriptions.Item label="Symbol">{data.symbol}</Descriptions.Item>
-                <Descriptions.Item label="Slug">{data.slug}</Descriptions.Item>
-                <Descriptions.Item label="Num Market Pairs">{data.num_market_pairs}</Descriptions.Item>
-                <Descriptions.Item label="Date Added">{new Date(data.date_added).toLocaleDateString()}</Descriptions.Item>
-                {data.tags.length &&
-                    data.tags.map(tag => (
-                        <Descriptions.Item label={tag} key={tag}>
-                            <Badge status="success" text="True" />
-                        </Descriptions.Item>
-                    ))
-                }
-                <Descriptions.Item label="Max Supply">{data.max_supply}</Descriptions.Item>
-                <Descriptions.Item label="Circulating Supply">{data.circulating_supply}</Descriptions.Item>
-                <Descriptions.Item label="Total Supply">{data.total_supply}</Descriptions.Item>
-                <Descriptions.Item label="Cmc Rank">{data.cmc_rank}</Descriptions.Item>
-                <Descriptions.Item label="Last Updated">{new Date(data.last_updated).toLocaleDateString()}</Descriptions.Item>
-
-                <Descriptions.Item label="Date Added">{data.date_added}</Descriptions.Item>
-            </Descriptions>
-
-            <Descriptions title="Platform" bordered>
-                <Descriptions.Item label="Name">{data.platform.name}</Descriptions.Item>
-                <Descriptions.Item label="Symbol">{data.platform.symbol}</Descriptions.Item>
-                <Descriptions.Item label="Slug">{data.platform.slug}</Descriptions.Item>
-                <Descriptions.Item label="Token Address">{data.platform.token_address}</Descriptions.Item>
-            </Descriptions>
-
-            <Descriptions title="Quote USD" bordered>
-                <Descriptions.Item label="Price">{data.quote.USD.price}</Descriptions.Item>
-                <Descriptions.Item label="Volume 24 h">{data.quote.USD.volume_24h}</Descriptions.Item>
-                <Descriptions.Item label="Percent Change 1h">{data.quote.USD.percent_change_1h}</Descriptions.Item>
-                <Descriptions.Item label="Percent Change 24h">{data.quote.USD.percent_change_24h}</Descriptions.Item>
-                <Descriptions.Item label="Percent Change 7d">{data.quote.USD.percent_change_7d}</Descriptions.Item>
-                <Descriptions.Item label="Market Cap">{data.quote.USD.market_cap}</Descriptions.Item>
-                <Descriptions.Item label="Last Updated">{data.quote.USD.last_updated}</Descriptions.Item>
-            </Descriptions>
-            </>
-        );
-    }
-}
+import FormatDetails from '../Components/FormatDetails';
 
 const Details = () => {
     const { Meta } = Card;
@@ -76,7 +28,7 @@ const Details = () => {
                 <Skeleton loading={loadingData} active>
                     <Meta
                         title="Details of"
-                        description={<DetailFormatted data={details}/>}
+                        description={<FormatDetails data={details}/>}
                     />
                 </Skeleton>
             </Card>
